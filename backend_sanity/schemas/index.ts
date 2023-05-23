961131e0fab31d2a029d0928a28aa36c7dae108b
Patrick Louis <patricklouisw@gmail.com>
2023-05-23 11:34:04 -0400

    Add different schemas

--- backend_sanity/schemas/index.ts
+++ backend_sanity/schemas/index.ts
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import about from './about'
+import testimonials from './testimonials'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  testimonials,
+  about,
+  brands,
+  contact,
+  experiences,
+  skills,
+  workExperience,
+  works,
+]
